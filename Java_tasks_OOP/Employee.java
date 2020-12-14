7ad1a77a97c925b72209c733f59df5624b169aa5
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2020-12-14 16:14:58 +0300

    Add files via upload

---
+++ Java_tasks_OOP/Employee.java
@@ -0,0 +1,69 @@
+package com.mycompany.firsttask;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    public Employee(int id, String firstName, String lastName, int salary) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("Id should be > 0");
+        } else {
+            this.id = id;
+        }
+        if (salary < 0) {
+            throw new IllegalArgumentException("Salary should be >= 0");
+        } else {
+            this.salary = salary;
+        }
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getName() {
+        return firstName + ' ' + lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        if (salary < 0) {
+            throw new IllegalArgumentException("salary should be >= 0");
+        } else {
+            this.salary = salary;
+        }
+    }
+
+    public int getAnnualSalary() {
+        return salary * 12;
+    }
+
+    public int raiseSalary(int percent) {
+        int increasedSalary = (100 + percent) * salary / 100;
+        return increasedSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee[" +
+                "id=" + id +
+                ", name=" + firstName + ' ' + lastName +
+                ", salary=" + salary +
+                ']';
+    }
+}
