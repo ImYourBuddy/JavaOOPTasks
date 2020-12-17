af8ab28ea4b1d2aa09d663d15880d52f271d6063
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2020-12-17 09:28:01 +0300

    Add Container.java

---
+++ Java_tasks_OOP_2/ball/Container.java
@@ -0,0 +1,59 @@
+package com.mycompany.secondtask.ball;
+
+/**
+ * This class is helper for the ball class.It describes the container in which the ball moves.
+ */
+public class Container {
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+    public Container(int x1, int y1, int width, int height) {
+        if (width <= 0) {
+            throw new IllegalArgumentException("The width should be > 0");
+        } else if (height <= 0) {
+            throw new IllegalArgumentException("The height should be > 0");
+        } else {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x1 + width;
+            this.y2 = y1 + height;
+        }
+    }
+
+    public int getX1() {
+        return x1;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public int getWidth() {
+        return x2 - x1;
+    }
+
+    public int getHeight() {
+        return y2 - y1;
+    }
+
+    public boolean collidesWidth(Ball ball) {
+        double x1BallSize = ball.getX() - ball.getRadius();
+        double x2BallSize = ball.getX() + ball.getRadius();
+        double y1BallSize = ball.getY() - ball.getRadius();
+        double y2BallSize = ball.getY() + ball.getRadius();
+        if ((x1BallSize >= x1 && x2BallSize <= x2) && (y1BallSize >= y1 && y2BallSize <= y2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Container[(" + x1 + ',' + y1 +
+                "),(" + x2 + ',' + y2 +
+                ")]";
+    }
+}
