86158224b810e0139782ba777c1c6bb5b0d2487c
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2021-01-09 20:11:00 +0300

    Update Author.java

--- Java_tasks_OOP/Author.java
+++ Java_tasks_OOP/Author.java
@@ -1,5 +1,7 @@
 package com.mycompany.firsttask.library;
 
+import java.util.Objects;
+
 /**
  * This class describe the author of the book for Book class
  */
@@ -36,5 +38,27 @@ public class Author {
 
     public void setGender(char gender) {
         this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        boolean nameEquals = (name == null && author.name == null)
+                || (name != null && name.equals(author.name));
+        boolean emailEquals = (email == null && author.email == null)
+                || (email != null && email.equals(author.email));
+        boolean genderEquals = (gender == author.gender);
+        return nameEquals && emailEquals && genderEquals;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + (name == null ? 0 : name.hashCode());
+        result = 31 * result + (email == null ? 0 : email.hashCode());
+        result = 31 * result + (int) gender;
+        return result;
     }
 }
