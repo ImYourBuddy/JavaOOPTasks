fd24ec089da3613c91183bd1517dd14710a96430
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2021-01-09 20:12:28 +0300

    Update Rectangle.java

--- Java_tasks_OOP/Rectangle.java
+++ Java_tasks_OOP/Rectangle.java
@@ -1,5 +1,7 @@
 package com.mycompany.firsttask.shapes;
 
+import java.util.Objects;
+
 public class Rectangle {
     private float length = 1.0f;
     private float width = 1.0f;
@@ -8,7 +10,7 @@ public class Rectangle {
     }
 
     public Rectangle(float length, float width) {
-       if (Math.abs(length) < 0.000001 || length < 0.0) {
+        if (Math.abs(length) < 0.000001 || length < 0.0) {
             throw new IllegalArgumentException("The length should be > 0");
         } else if (Math.abs(length) < 0.000001 || length < 0.0) {
             throw new IllegalArgumentException("The width should be > 0");
@@ -44,16 +46,19 @@ public class Rectangle {
 
     /**
      * Calculate the area of a rectangle
+     *
      * @return the area of a rectangle
      */
-    public double getArea () {
+    public double getArea() {
         return length * width;
     }
+
     /**
      * Calculate the perimeter of a rectangle
+     *
      * @return the perimeter of a rectangle
      */
-    public double getPerimeter () {
+    public double getPerimeter() {
         return 2 * length + 2 * width;
     }
 
@@ -63,5 +68,23 @@ public class Rectangle {
                 "length=" + length +
                 ", width=" + width +
                 ']';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        boolean lengthEquals = (Math.abs(length - rectangle.length) < 0.000001);
+        boolean widthEquals = (Math.abs(width - rectangle.width) < 0.000001);
+        return lengthEquals && widthEquals;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + Float.hashCode(length);
+        result = 31 * result + Float.hashCode(width);
+        return result;
     }
 }
