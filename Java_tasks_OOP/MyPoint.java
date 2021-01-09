5ef91287048116a038a50e681198bbf7669beae8
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2021-01-09 20:13:10 +0300

    Update MyPoint.java

--- Java_tasks_OOP/MyPoint.java
+++ Java_tasks_OOP/MyPoint.java
@@ -1,5 +1,7 @@
 package com.mycompany.firsttask.triangle;
 
+import java.util.Objects;
+
 public class MyPoint {
     private int x = 0;
     private int y = 0;
@@ -68,5 +70,22 @@ public class MyPoint {
      */
     public double distance() {
         return this.distance(0, 0);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyPoint myPoint = (MyPoint) o;
+        return x == myPoint.x &&
+                y == myPoint.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + x;
+        result = 31 * result + y;
+        return result;
     }
 }
