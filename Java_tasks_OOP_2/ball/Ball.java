be7c15c9cc0baf23d8eb7551e9ea43b15022e9c3
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2021-01-09 20:14:49 +0300

    Update Ball.java

--- Java_tasks_OOP_2/ball/Ball.java
+++ Java_tasks_OOP_2/ball/Ball.java
@@ -1,5 +1,7 @@
 package com.mycompany.secondtask.ball;
 
+import java.util.Objects;
+
 public class Ball {
     private float x;
     private float y;
@@ -87,5 +89,28 @@ public class Ball {
                 "," + y + ')' +
                 ", speed=(" + xDelta + ',' + yDelta +
                 ")]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        boolean xEquals = (Math.abs(x - ball.x) < 0.000001);
+        boolean yEquals = (Math.abs(y - ball.y) < 0.000001);
+        boolean xDeltaEquals = (Math.abs(xDelta - ball.xDelta) < 0.000001);
+        boolean yDeltaEquals = (Math.abs(yDelta - ball.yDelta) < 0.000001);
+        return radius == ball.radius && xEquals && yEquals && xDeltaEquals && yDeltaEquals;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + Float.hashCode(x);
+        result = 31 * result + Float.hashCode(y);
+        result = 31 * result + Float.hashCode(xDelta);
+        result = 31 * result + Float.hashCode(yDelta);
+        result = 31 * result + radius;
+        return result;
     }
 }
