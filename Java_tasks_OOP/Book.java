6c2d6552fa9b5f986894f561fc791b3793a8bda9
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2021-01-09 20:11:41 +0300

    Update Book.java

--- Java_tasks_OOP/Book.java
+++ Java_tasks_OOP/Book.java
@@ -1,5 +1,9 @@
 package com.mycompany.firsttask.library;
 
+
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Book {
     private String name;
     private Author[] authors;
@@ -7,7 +11,7 @@ public class Book {
     private int qty = 0;
 
     public Book(String name, Author[] authors, double price) {
-       if (price < 0) {
+        if (price < 0) {
             throw new IllegalArgumentException("The price should be >= 0");
         } else {
             this.price = price;
@@ -81,5 +85,28 @@ public class Book {
                 ", price=" + price +
                 ", qty=" + qty +
                 ']';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        boolean priceEquals = (Math.abs(price - book.price) < 0.000001);
+        boolean qtyEquals = (qty == book.qty);
+        boolean nameEquals = (name == null && book.name == null)
+                || (name != null && name.equals(book.name));
+        boolean authorsEquals = Arrays.equals(authors, book.authors);
+        return priceEquals && qtyEquals && nameEquals && authorsEquals;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + Double.hashCode(price);
+        result = 31 * result + qty;
+        result = 31 * result + (name == null ? 0 : name.hashCode());
+        result = 31 * result + Arrays.hashCode(authors);
+        return result;
     }
 }
